cfdf668a86c15d628d507fa436ed8d5408e0a631
Chris Wailes <chris.wailes@gmail.com>
2012-04-09 18:19:54 -0600

    Added a bit to the value.rb file and moved the Instruction class and its subclasses into the instruction.rb file.  This should now be a complete first draft.

---
+++ lib/rltk/cg/instruction.rb
@@ -0,0 +1,236 @@
+# Author:		Chris Wailes <chris.wailes@gmail.com>
+# Project: 	Ruby Language Toolkit
+# Date:		2012/04/09
+# Description:	This file defines LLVM Instruction classes.
+
+############
+# Requires #
+############
+
+# Ruby Language Toolkit
+require 'rltk/cg/bindings'
+require 'rltk/cg/value'
+
+#######################
+# Classes and Modules #
+#######################
+
+module RLTK::CG
+	class Instruction < User
+		
+		TESTABLE = [
+			:Alloca,
+			:BitCast,
+			:Call,
+			:ExtractElement,
+			:ExtractValue,
+			:FPCmp,
+			[:FPExtend, :FPExt],
+			:FPToSI,
+			:FPToUI,
+			:FPTrunc,
+			:GetElementPtr,
+			[:IntCmp, :ICmp],
+			:InsertElement,
+			:InsertValue,
+			:IntToPtr,
+			:Invoke,
+			:Load,
+			:PtrToInt,
+			:Return,
+			[:SignExtend, :SExt],
+			:SIToFP,
+			:Select,
+			:ShuffleVector,
+			:Store,
+			:Switch,
+			[:Truncate, :Trunc],
+			:UIToFP,
+			:Unreachable
+		]
+		
+		def self.from_ptr(ptr)
+			match = nil
+			
+			TESTABLE.each do |el|
+				klass, test =
+				if el.is_a?(Symbol)
+					RLTK::CG.const_get("#{el}Inst".to_sym), Bindings.get_bname("IsA#{el}Inst")
+					
+				elsif el.is_a?(Array)
+					RLTK::CG.const_get("#{el.first}Inst".to_sym), Bindings.get_bname("IsA#{el.last}Inst")
+				end
+				
+				match = klass if Bindings.send(test, ptr)
+			end
+			
+			if match then match else Intruction end.new(ptr)
+		end
+		
+		# You should never instantiate Instruction object directly.  Use the
+		# builder class to add new instructions.
+		def initialize(ptr)
+			raise 'The ptr argument must be an instance of the FFI::Pointer class.' if not ptr.is_a?(FFP::Pointer)
+			
+			@ptr = ptr
+		end
+		
+		def next
+			if (next_ptr = Bindings.get_next_instruction(@ptr)).null?
+				nil
+			else
+				Instruction.from_ptr(next_ptr)
+			end
+		end
+		
+		def parent
+			if (parent_ptr = Bindings.get_instruction_parent(@ptr)).null?
+				nil
+			else
+				BasicBlock.new(parent_ptr)
+			end
+		end
+		
+		def previous
+			if (prev_ptr = Bindings.get_previous_instruction(@ptr)).null?
+				nil
+			else
+				Instruction.from_ptr(prev_ptr)
+			end
+		end
+		
+		##########################################
+		# Instruction Testing Method Definitions #
+		##########################################
+		
+		selector	= Regexp.new(/LLVMIsA.*Inst/)
+		syms		= Symbol.all_symbols.select { |sym| selector.match(sym.to_s) }
+		
+		syms.each do |sym|
+			sym = (Bindings.get_bname(sym).to_s + '?').to_sym
+			
+			define_method(sym)
+				Bindings.send(sym, @ptr)
+			end
+		end
+	end
+	
+	class CallInst < Instruction
+		def calling_convention
+			Bindings.get_instruction_call_conv(@ptr)
+		end
+		
+		def calling_convention=(conv)
+			Bindings.set_instruction_call_conv(@ptr, conv)
+			
+			conv
+		end	
+	end
+	
+	class PhiInst < Instruction
+		def add_incoming(incoming)
+			blks = incoming.keys
+			vals = incoming.values_at(*blks)
+			
+			FFI::MemoryPointer.new(FFI.type_size(:pointer) * incoming.size) do |vals_ptr|
+				
+				vals_ptr.write_array_of_pointers(vals)
+				
+				FFI::MemoryPointer.new(FFI.type_size(:pointer) * incoming.size) do |blks_ptr|
+					blks_ptr.write_array_of_pointers(blks)
+					
+					Bindings.add_incoming(@ptr, vals_ptr, blks_ptr, vals.length)
+				end
+			end
+			
+			nil
+		end
+	end
+	
+	class SwitchInst < Instruction
+		def add_case(val, block)
+			Bindings.add_case(@ptr, val, block)
+		end
+	end
+	
+	# Empty Instruction Classes
+	
+	class AddInst				< Instruction; end
+	class AllocaInst			< Instruction; end
+	class AndInst				< Instruction; end
+	class ARightShiftInst		< Instruction; end
+	class ArrayAllocaInst		< Instruction; end
+	class ArrayMallocInst		< Instruction; end
+	class BitCastInst			< Instruction; end
+	class BranchInst			< Instruction; end
+	class CondBranchInst		< Instruction; end
+	class ExactSDivInst			< Instruction; end
+	class ExtractElementInst		< Instruction; end
+	class ExtractValueInst		< Instruction; end
+	class FAddInst				< Instruction; end
+	class FDivInst				< Instruction; end
+	class FMulInst				< Instruction; end
+	class FNegInst				< Instruction; end
+	class FPToSIInst			< Instruction; end
+	class FPToUIInst			< Instruction; end
+	class FPCastInst			< Instruction; end
+	class FPCmpInst			< Instruction; end
+	class FPExtendInst			< Instruction; end
+	class FPTruncInst			< Instruction; end
+	class FreeInst				< Instruction; end
+	class FRemInst				< Instruction; end
+	class FSubInst				< Instruction; end
+	class GetElementPtrInst		< Instruction; end
+	class GlobalStringInst		< Instruction; end
+	class GlobalStringPtrInst	< Instruction; end
+	class InBoundsGEPInst		< Instruction; end
+	class InsertElementInst		< Instruction; end
+	class InsertValueInst		< Instruction; end
+	class IntToPtrInst			< Instruction; end
+	class IntCastInst			< Instruction; end
+	class IntCmpInst			< Instruction; end
+	class InvokeInst			< Instruction; end
+	class IsNotNullInst			< Instruction; end
+	class IsNullInstInst		< Instruction; end
+	class LeftShiftInst			< Instruction; end
+	class LoadInst				< Instruction; end
+	class LRightShiftInst		< Instruction; end
+	class MallocInst			< Instruction; end
+	class MulInst				< Instruction; end
+	class NegInst				< Instruction; end
+	class NotInst				< Instruction; end
+	class NSWAddInst			< Instruction; end
+	class NSWMulInst			< Instruction; end
+	class NSWNegInst			< Instruction; end
+	class NSWSubInst			< Instruction; end
+	class NUWAddInst			< Instruction; end
+	class NUWMulInst			< Instruction; end
+	class NUWNegInst			< Instruction; end
+	class NUWSubInst			< Instruction; end
+	class OrInst				< Instruction; end
+	class PtrToIntInst			< Instruction; end
+	class PtrCastInst			< Instruction; end
+	class PtrDiffInst			< Instruction; end
+	class ReturnInst			< Instruction; end
+	class ReturnAggregateInst	< Instruction; end
+	class ReturnVoidInst		< Instruction; end
+	class SDivInst				< Instruction; end
+	class SelectInst			< Instruction; end
+	class ShuffleVectorInst		< Instruction; end
+	class SignExtendInst		< Instruction; end
+	class SignExtendOrBitCastInst	< Instruction; end
+	class SIToFPInst			< Instruction; end
+	class SRemInst				< Instruction; end
+	class StoreInst			< Instruction; end
+	class StructGEPInst			< Instruction; end
+	class SubInst				< Instruction; end
+	class TruncateInst			< Instruction; end
+	class TruncateOrBitCastInst	< Instruction; end
+	class UDivInst				< Instruction; end
+	class UIToFPInst			< Instruction; end
+	class UnreachableInst		< Instruction; end
+	class URemInst				< Instruction; end
+	class XOrInst				< Instruction; end
+	class ZeroExtendInst		< Instruction; end
+	class ZeroExtendOrBitCastInst	< Instruction; end
+end
--- lib/rltk/cg/value.rb
+++ lib/rltk/cg/value.rb
@@ -9,40 +9,121 @@
 
 # Ruby Language Toolkit
 require 'rltk/cg/bindings'
+require 'rltk/cg/type'
 
 #######################
 # Classes and Modules #
 #######################
 
 module RLTK::CG
-	class Value
-		def initialize(ptr)
-			if not ptr.is_a?(FFI::Pointer)
-				raise 'Argument to new should be of class FFI::Pointer'
-			end
+	class Value < BindingClass
+		attr_reader :attributes
+		
+		def initialize
+			raise 'The Value class can not be instantiated directly.' if self.class == Value
 			
-			if ptr.null?
-				raise NullPointerError, 'Trying to create a Value with a null pointer.'
-			end
-			
-			@ptr = ptr
+			@attributes = AttrCollection.new(self)
 		end
 		
+		def add_attribute(attribute)
+			Bindings.add_attribute(@ptr, attribute)
+		end
+		
+		def constant?
+			Bindings.is_constant(@ptr).to_bool
+		end
+		
+		def dump
+			Bindings.dump_value(@ptr)
+		end
+		
+		def hash
+			@ptr.address.hash
+		end
+		
+		def name
+			Bindings.get_value_name(@ptr)
+		end
+		
+		def name=(str)
+			raise 'The str parameter must be a String.' if not str.instance_of?(String)
+			
+			Bindings.set_value_name(@ptr, str)
+			
+			return str
+		end
+		
+		def null?
+			Bindings.is_null(@ptr).to_bool
+		end
+		
+		# FIXME Is this even needed?
 		def type
-			type_of(@ptr)
+		end
+		
+		def undefined?
+			Bindings.is_undef(@ptr).to_bool
+		end
+		
+		class AttrCollection
+			@@add_method = :add_attribute
+			@@del_method = :remove_attribute
+			
+			def initialize(value)
+				@attributes	= Array.new
+				@value		= value
+			end
+			
+			def add_attribute(attribute)
+				if not @attributes.include?(attribute)
+					@attributes << attribute
+					Bindings.send(@@add_method, @value.to_ptr, attribute)
+				end
+			end
+			alias :'<<' :add_attribute
+			
+			def include?(attribute)
+				@attributes.include?(attribute)
+			end
+			
+			def remove_attribute(attribute)
+				if @attributes.include?(attribute)
+					@attributes.delete(attribute)
+					Bindings.send(@@del_method, @value.to_ptr, attribute)
+				end
+			end
+			alias :'>>' :remove_attribute
+			
+			def to_s
+				@attributes.to_s
+			end
 		end
 	end
 	
 	class Argument < Value
 	end
 	
-	class Basicblock < Value
-	end
-	
 	class User < Value
 	end
 	
 	class Constant < User
+		def initialize(kind, type)
+			raise 'The type parameter must be an instance of the RLTK::CG::Type class.' if not type.is_a?(Type)
+			
+			@ptr =
+			case kind
+			when :null	then Bindings.const_null(type)
+			when :null_ptr	then Bindings.const_pointer_null(type)
+			when :undef	then Bindings.get_undef(type)
+			else raise 'Constants must be of kind :null, :null_ptr, or :undef.'
+			end
+		end
+		
+		def bitcast_to(type)
+		end
+		
+		def get_element_ptr(*indices)
+		end
 	end
 	
 	class ConstantArray < Constant
@@ -70,24 +151,67 @@ module RLTK::CG
 	end
 	
 	class GlobalValue < Constant
+		def alignment
+			Bindings.get_alignment(@ptr)
+		end
+		
+		def alignment=(bytes)
+			Bindings.set_alignment(@ptr, bytes)
+		end
+		
+		def declaration?
+			Bindings.is_declaration(@ptr)
+		end
+		
+		def global_constant?
+			Bindings.is_global_constant(@ptr)
+		end
+		
+		def global_constant=(flag)
+			Bindings.set_global_constant(@ptr, flag)
+		end
+		
+		def initializer=(val)
+			raise 'The val parameter must be of type RLTK::CG::Value.' if not val.is_a?(Value)
+			
+			Bidnings.set_initializer(@ptr, val)
+		end
+		
+		def linkage
+			Bindings.get_linkage(@ptr)
+		end
+		
+		def linkage=(linkage)
+			Bindings.set_linkage(@ptr, linkage)
+		end
+		
+		def section
+			Bindings.get_section(@ptr)
+		end
+		
+		def section=(section)
+			Bindings.set_section(@ptr, section)
+		end
+		
+		def visibility
+			Bindings.get_visibility(@ptr)
+		end
+		
+		def visibility=(vis)
+			Bindings.set_visibility(@ptr, vis)
+		end
 	end
 	
 	class GlobalAlias < GlobalValue
 	end
 	
 	class GlobalVariable < GlobalValue
-	end
-	
-	class Instruction < User
-	end
-	
-	class CallInst < Instruction
-	end
-	
-	class Phi < Instruction
-	end
-	
-	class SwitchInst < Instruction
+		def thread_local?
+			Bindings.is_thread_local(@ptr).to_bool
+		end
+		
+		def thread_local=(local)
+			Bindings.set_thread_local(@ptr, local.to_i)
+		end
 	end
 end
-
