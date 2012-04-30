78d1c56cb2ac30314458d31198baaaca655c588e
Chris Wailes <chris.wailes@gmail.com>
2012-04-30 16:37:57 -0600

    Added some more utility methods to the monkeys.rb file.

--- lib/rltk/util/monkeys.rb
+++ lib/rltk/util/monkeys.rb
@@ -7,6 +7,38 @@
 # Requires #
 ############
 
+###########
+# Methods #
+###########
+
+def check_type(o, type, blame = nil, strict = false)
+	type_ok = if strict then o.isntance_of?(type) else o.is_a?(type) end
+	
+	if type_ok
+		o
+	else
+		if blame
+			raise "Parameter #{blame} must be an instance of the #{type.name} class.  Received an instance of #{o.class.name}."
+		else
+			raise "Expected an object of type #{type.name}.  Received an instance of #{o.class.name}."
+		end
+	end
+end
+
+def check_array_type(array, type, blame = nil, strict = false)
+	array.each do |o|
+		type_ok = if strict then o.instance_of?(type) else o.is_a?(type) end
+		
+		if not type_ok
+			if blame
+				raise "Parameter #{blame} must contain instances of the #{type.name} class."
+			else
+				raise "Expected an object of type #{type.name}."
+			end
+		end
+	end
+end
+
 #######################
 # Classes and Modules #
 #######################
@@ -15,6 +47,26 @@ class Object
 	def returning(value)
 		yield(value)
 		value
+	end
+end
+
+class Class
+	def includes_module?(mod)
+		self.included_modules.include?(mod)
+	end
+	
+	def short_name
+		self.name.split('::').last
+	end
+	
+	def subclass_of?(klass)
+		raise "The klass parameter must be an instance of Class." if not klass.is_a?(Class)
+		
+		if (superklass = self.superclass)
+			superklass == klass or superklass.subclass_of?(klass)
+		else
+			false
+		end
 	end
 end
 
