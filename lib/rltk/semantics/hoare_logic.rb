1a7f87dc1f03a89a58016d269b81a2ed50bcc02e
Chris Wailes <chris.wailes@gmail.com>
2012-07-27 12:38:11 -0600

    Starting work on adding tools for working with formal semantics to RLTK.

---
+++ lib/rltk/semantics/hoare_logic.rb
@@ -0,0 +1,19 @@
+# Author:		Chris Wailes <chris.wailes@gmail.com>
+# Project: 	Ruby Language Toolkit
+# Date:		2012/07/27
+# Description:	This file contains a class working with Hoare Logic.
+
+############
+# Requires #
+############
+
+#######################
+# Classes and Modules #
+#######################
+
+module RLTK::Semantics # :nodoc:
+	
+	class HoareLogic < Axiomatic
+		
+	end
+end
---
+++ lib/rltk/semantics/lambda_calculus.rb
@@ -0,0 +1,19 @@
+# Author:		Chris Wailes <chris.wailes@gmail.com>
+# Project: 	Ruby Language Toolkit
+# Date:		2012/07/27
+# Description:	This file contains a class for working with the Lambda Calculus.
+
+############
+# Requires #
+############
+
+#######################
+# Classes and Modules #
+#######################
+
+module RLTK::Semantics # :nodoc:
+	
+	class LambdaCalculus
+		
+	end
+end
