766f5d0b8dbff90644b750d4aac129769f3be1f9
dbarbera <diego@dbarbera.name>
2013-05-02 13:14:56 +0200

    Added attribute readers to LexingError

--- lib/rltk/lexer.rb
+++ lib/rltk/lexer.rb
@@ -22,6 +22,17 @@ module RLTK # :nodoc:
 	# A LexingError exception is raised when an input stream contains a
 	# substring that isn't matched by any of a lexer's rules.
 	class LexingError < StandardError
+		# @return [Integer]
+		attr_reader :stream_offset
+
+		# @return [Integer]
+		attr_reader :line_number
+
+		# @return [Integer]
+		attr_reader :line_offset
+
+		# @return [String]
+		attr_reader :remainder
 		
 		# @param [Integer]	stream_offset	Offset from begnning of string.
 		# @param [Integer]	line_number	Number of newlines encountered so far.
