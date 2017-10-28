9ce0e8008011b73e7c8e57d7ba36a611cd9af991
Phil <pnomolos@gmail.com>
2017-10-28 11:38:41 -0700

    Add ability to limit amount of context returned for error message

--- lib/rltk/parser.rb
+++ lib/rltk/parser.rb
@@ -34,6 +34,16 @@ module RLTK
 	# defined language.
 	class NotInLanguage < StandardError
 
+		class << self
+			def default_context_length
+				@default_context_length || 100
+			end
+
+			def default_context_length=(v)
+				@default_context_length = v
+			end
+		end
+
 		# @return [Array<Token>]  List of tokens that have been successfully parsed
 		attr_reader :seen
 
@@ -46,15 +56,18 @@ module RLTK
 		# @param [Array<Token>]  seen       Tokens that have been successfully parsed
 		# @param [Token]         current    Token that caused the parser to stop
 		# @param [Array<Token>]  remaining  Tokens that have yet to be seen
-		def initialize(seen, current, remaining)
-			@seen      = seen
-			@current   = current
-			@remaining = remaining
+		def initialize(seen, current, remaining, context_length = self.class.default_context_length)
+			@seen           = seen
+			@current        = current
+			@remaining      = remaining
+			@context_length = context_length
 		end
 
 		# @return [String] String representation of the error.
 		def to_s
-			"String not in language.  Token info:\n\tSeen: #{@seen}\n\tCurrent: #{@current}\n\tRemaining: #{@remaining}"
+			seen = @context_length == :all ? @seen : @seen[-@context_length..-1]
+			remaining = @context_length == :all ? @remaining : @remaining[0..@context_length]
+			"String not in language.  Token info:\n\tSeen: #{seen}\n\tCurrent: #{@current}\n\tRemaining: #{remaining}"
 		end
 	end
 
